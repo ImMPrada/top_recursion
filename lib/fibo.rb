45b6c13ee72a2e6f83330db00d9d0eccbae82d68
immprada <im.mprada@gmail.com>
2022-09-12 15:14:56 -0500

    add logic for all cases

--- lib/fibo.rb
+++ lib/fibo.rb
@@ -1,3 +1,13 @@
+# f0 = 0
+# f1 = 1
+# f2 = 1 = f1 + f0
+# f3 = 2 = f2 + f1
+# f4 = 3 = f3 + f2
+# f5 = 5 = f4 + f3
+# f6 = 8 = f5 + f4
+# f7 = 13 = f6 + f5
+# f8 = 21 = f7 + f6
+
 module Lib
   class Fibo
     def initialize; end
@@ -11,6 +21,7 @@ module Lib
     def fibo(number)
       return number if number <= 1
 
+      fibo(number - 1) + fibo(number - 2)
     end
   end
 end
--- spec/lib/fibo_spec.rb
+++ spec/lib/fibo_spec.rb
@@ -5,13 +5,31 @@ RSpec.describe Lib::Fibo do
 
   describe 'when number is: 0' do
     it 'should return 1' do
-      expect(factorial.calc_for(0)).to eq(1)
+      expect(fibo.calc_for(0)).to eq(0)
     end
   end
 
   describe 'when number is: 1' do
     it 'should return 1' do
-      expect(factorial.calc_for(1)).to eq(1)
+      expect(fibo.calc_for(1)).to eq(1)
+    end
+  end
+
+  describe 'when number is: 2' do
+    it 'should return 1' do
+      expect(fibo.calc_for(2)).to eq(1)
+    end
+  end
+
+  describe 'when number is: 3' do
+    it 'should return 2' do
+      expect(fibo.calc_for(3)).to eq(2)
+    end
+  end
+
+  describe 'when number is: 5' do
+    it 'should return 5' do
+      expect(fibo.calc_for(5)).to eq(5)
     end
   end
 end
